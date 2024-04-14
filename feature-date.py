594f29e2e609adee7d49c6b367c0f3bcfdeb50c0
huarq28 <67198639+huarq28@users.noreply.github.com>
2024-04-14 12:33:58 -0400

    modify (#6)

--- feature-date.py
+++ feature-date.py
@@ -1,22 +1,22 @@
 import pandas as pd
-import numpy as np
+
 
 def cyclical_encode_dates(df):
     """
-    Enhances a DataFrame by converting specified date columns to datetime format, reporting missing values,
-    and adding cyclical features for month and day of year.
+    Enhances a DataFrame by converting specified date columns to datetime format and calculating the number
+    of days passed since each date.
 
     Parameters:
     - df (DataFrame): Input DataFrame with 'first_review', 'host_since', 'last_review' columns.
 
     Returns:
-    - DataFrame: Modified DataFrame with new cyclical feature columns for month and day.
-    
+    - DataFrame: Modified DataFrame with new columns showing days passed since each specified date.
+
     New columns added for each date column include:
-    - [column_name]_month, [column_name]_year, [column_name]_day_of_year,
-      [column_name]_month_sin, [column_name]_month_cos, [column_name]_day_sin, [column_name]_day_cos.
+    - [column_name]_days_since
     """
     date_columns = ['first_review', 'host_since', 'last_review']
+    current_date = pd.to_datetime("today")
     
     for col in date_columns:
         missing_count = df[col].isna().sum()
@@ -26,14 +26,8 @@ def cyclical_encode_dates(df):
         df[col] = pd.to_datetime(df[col], errors='coerce')  # Coerce errors will turn problematic parsing into NaT
 
     for col in date_columns:
-        df[col + '_month'] = df[col].dt.month
-        df[col + '_year'] = df[col].dt.year
-        df[col + '_day_of_year'] = df[col].dt.dayofyear
-        
-        df[col + '_month_sin'] = np.sin(2 * np.pi * df[col + '_month'] / 12)
-        df[col + '_month_cos'] = np.cos(2 * np.pi * df[col + '_month'] / 12)
-        df[col + '_day_sin'] = np.sin(2 * np.pi * df[col + '_day_of_year'] / 365)
-        df[col + '_day_cos'] = np.cos(2 * np.pi * df[col + '_day_of_year'] / 365)
+        df[col + '_days_since'] = (current_date - df[col]).dt.days
+
 
     return df
 
