31a81445508ff3d6e3d823319bab4cc0babfeeb8
Chan WingYiu <ChanWingYiu@dhcp-vl2051-959.eduroam.cornell.edu>
2024-04-14 11:11:49 -0400

    WIP:

--- categorical.py
+++ categorical.py
@@ -6,15 +6,14 @@ def one_hot_encode_categorical(df):
     categorical_group = ['property_type', 'room_type', 'bed_type', 'cancellation_policy', 'cleaning_fee', 'host_has_profile_pic', 'host_identity_verified', 'instant_bookable']
     
     # drop missing value 
-    df_dropped = df.dropna(subset=categorical_group)
+    # df_dropped = df.dropna(subset=categorical_group)
+    df[categorical_group].fillna("Unknown", inplace=True)
 
     # Setting up the OneHotEncoder
     encoder = OneHotEncoder(dtype=bool)
     transformer = ColumnTransformer([('one_hot_encoder', encoder, categorical_group)], remainder='passthrough')
     
     # Applying one-hot encoding
-    encoded_array = transformer.fit_transform(df_dropped)
+    encoded_array = transformer.fit_transform(df)
     encoded_df = pd.DataFrame(encoded_array, columns=transformer.get_feature_names_out())
     return encoded_df
-
-res = one_hot_encode_categorical(df)
